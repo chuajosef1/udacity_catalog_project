78b5ca315e6edd2c56226d2601ff9be9a16a3f0b
Josef Chua <chuajosef1@gmail.com>
2019-04-04 16:34:01 -0700

    feat: Create addNewItem function
    Looks for a post request to commit a
    new item to the db.

--- application.py
+++ application.py
@@ -25,12 +25,30 @@ session = DBSession()
 @app.route('/')
 @app.route('/catalog')
 def showCatalog():
-    categories = session.query(Categories).order_by(asc(Categories.name))
+    categories = session.query(Categories).all()
     items = session.query(CategoryItem).order_by(
         CategoryItem.id.desc()).limit(10)
-
     return render_template('showCatalog.html', categories=categories, items=items)
 
 
+'''
+user_id temporary until login is added.
+'''
+@app.route('/catalog/newItem/', methods=['GET', 'POST'])
+def addNewItem():
+    if request.method == 'POST':
+        newItem = CategoryItem(
+            name=request.form['name'],
+            description=request.form['description'],
+            category_id=request.form['category_id'])
+        session.add(newItem)
+        session.commit()
+        flash('New item added')
+        return redirect(url_for('showCatalog'))
+    else:
+        return render_template('newItem.html')
+
+
 if __name__ == '__main__':
+    app.secret_key = 'this-is-my-secret'
     app.run(host='0.0.0.0', port=8000)
