ad866b94141fe3a98836ba83249b8c6ab3f6687f
Josef <chuajosef1@gmail.com>
2019-05-09 20:54:49 -0700

    Update populatedatabase.py

--- populatedatabase.py
+++ populatedatabase.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Categories, CategoryItem, User
 
 # Create database and create a shortcut for easier to update database
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
